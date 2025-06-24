f372cd75f195ab1651e4acc7c3ea2bf2b820f08e
karmadecoded <mashunea@gmail.com>
2025-06-24 19:26:07 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,7 +1,7 @@
 // firebase-messaging-sw.js
 
-importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyAPCFjjC6SqtQEauTQ6Hs7Ex-B2tj6PuXM",
