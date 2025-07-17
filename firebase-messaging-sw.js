e18f6143073fd72fbde461aa4b003f1e3df03d5f
karmadecoded <mashunea@gmail.com>
2025-07-17 12:07:51 +0100

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,39 @@
+importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/11.9.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAPCFjjC6SqtQEauTQ6Hs7Ex-B2tj6PuXM",
+  authDomain: "soul-code-app.firebaseapp.com",
+  projectId: "soul-code-app",
+  storageBucket: "soul-code-app.firebasestorage.app",
+  messagingSenderId: "339179205157",
+  appId: "1:339179205157:web:e801c0ad054cbc6a1c05cc",
+  measurementId: "G-C1K5PGZJB7"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+    return self.registration.showNotification('SoulCode', {
+        body: payload.notification.body,
+        icon: '/icon-192.png',
+        badge: '/icon-192.png',
+        tag: 'soulcode-affirmation'
+    });
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({type: 'window', includeUncontrolled: true})
+            .then(function(clientList) {
+                if (clientList.length > 0) {
+                    let client = clientList[0];
+                    client.focus();
+                    client.postMessage({type: 'OPEN_RECENT_AFFIRMATIONS'});
+                    return;
+                }
+                clients.openWindow('/?page=recent-affirmations');
+            })
+    );
+});
